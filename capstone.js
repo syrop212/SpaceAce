9498d2a1ead9bab9dee31363c35ffb266b1ab5ff
Carlos Casas <albertocasas05@gmail.com>
2017-05-04 09:04:41 -0700

    uh

--- capstone.js
+++ capstone.js
@@ -9,11 +9,23 @@ var player = makeImage("images/raiser.png",10, 50, 150, 150, 1)
 
 var enemies =[]
 
+var points = [10, 20, 30, 40]
+
 var powerups = []
 
 var missiles = []
 
+var shot = []
+
 var gameOver = false
+
+var keyState = {};    
+window.addEventListener('keydown',function(e){
+    keyState[e.keyCode || e.which] = true;
+},true);    
+window.addEventListener('keyup',function(e){
+    keyState[e.keyCode || e.which] = false;
+},true);
 
 function drawPlayer(){
     setX(player, mouse.x)
@@ -46,16 +58,17 @@ function drawEnemies() {
         if(getX(enemies[i]) > -100) {
             move(enemies[i], -3, 0)
             
-           if(oldscore = oldscore*2){
-                move(enemies[i], -score*2, 0)
+          for(var j = 0; j < points.length; j++){    
+           if(oldscore <= points[j]) {
+                move(enemies[i], -oldscore*2 , 0)
                  }
+          }
         }
         else {
          setX(enemies[i], 850)
          setY(enemies[i], random(0, 500))
         }
-    }
-  
+  }
 }
 
 function fireMIssile(e) {
@@ -63,28 +76,23 @@ function fireMIssile(e) {
     var beamx = getX(player) + 150 - 5
     var beamy = getY(player) + 150/2
     var beam = makeRect(beamx, beamy, 50, 5, "lime", 1)
+    
     missiles.push(beam)
     }
 }
 
-var keyState = {};    
-window.addEventListener('keydown',function(e){
-    keyState[e.keyCode || e.which] = true;
-},true);    
-window.addEventListener('keyup',function(e){
-    keyState[e.keyCode || e.which] = false;
-},true);
-
-
 function drawMissle() {
     for(var i = 0; i < missiles.length; i++) {
-        move(missiles[i], 20, 0)
+       // for(var j = 0; shot.length; j++){
+        move(missiles[i], 20,0)    
+        //move(shot[j], 10, 0)
+          //} 
         }
     }
 
 function makePower(){
  var p = makeImage("images/Dust.png", 1000, random(10,500), 50, 50, 1) 
- var o = makeImage("images/daily.png", 1100, random(20, 400), 50, 50, 1)
+ var o = makeImage("images/daily.png", 1100, random(20, 400), 30, 30, 1)
  powerups.push(p, o)
  setTimeout(makePower, 3000)
 }
@@ -95,7 +103,7 @@ function drawPowerUp() {
         move(powerups[i], -10, 0)
         }
         else {
-         setX(powerups[i], 850)
+         setX(powerups[i], 1000)
          setY(powerups[i], random(0, 500))
         }
         }
@@ -115,10 +123,11 @@ function checkCollisionsPlayer() {
        for(var j = 0; j < powerups.length; j++){
            
         if(collide(player, powerups[j], 0, 0) == true){
-            var beam2 = makeRect(beamx, beamy - 20, 80, 20, "red", 1)
-            var beam3 = makeRect(beamx, beamy + 20, 10, 90, "pink", 1)
+            var beam2 = makeRect(beamx, beamy - 5, 80, 20, "red", 1)
+           // var beam3 = makeRect(beamx, beamy - 10, 10, 95, "pink", 1)
             removeArrayElement(powerups, j)
-            missiles.push(beam2, beam3)
+            missiles.push(beam2)
+            //shot.push(beam3)
         }
        }
   }
